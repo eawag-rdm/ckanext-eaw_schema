7691076b70b23ae48c2fc53fd7adb3480b7c4963
Harald von Waldow <harald.vonwaldow@eawag.ch>
2017-07-05 19:33:50 +0200

    WIP

--- ckanext/eaw_schema/fanstatic/eaw_schema_publication.js
+++ ckanext/eaw_schema/fanstatic/eaw_schema_publication.js
@@ -9,10 +9,10 @@ ckan.module('eaw_schema_publication', function ($) {
       var module = this;
       var $el = this.el;
       var publink = $( 'div.publicationcheck_wrap' ).nextAll('div').first();
+      publink.css("display", "none");
       $el.change(function() {
 	if ($el.prop("checked")) {
 	  publink.css("display", "block").css("visibility", "visible")
-	  .css("background", "#C4EBEA").css("border-radius", "10px");
 	} else {
 	  publink.css("visibility", "hidden").css("display", "block");
 	}
