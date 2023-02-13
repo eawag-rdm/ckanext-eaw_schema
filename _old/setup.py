f449c512855503bc0a5f153efd523f82baa5e772
Christian Foerster <christian.foerster@eawag.ch>
2023-02-13 10:58:07 +0000

    fresh setup

--- _old/setup.py
+++ _old/setup.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
 from codecs import open  # To use a consistent encoding
 from os import path
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -20,14 +19,13 @@ setup(
 
     description='''Eawag dataset schema. Builds on ckanext-scheming and ckanext-repeating.''',
     long_description=long_description,
-    long_description_content_type="text/markdown",
 
     # The project's main homepage.
-    url='https://github.com/eawag-rdm/ckanext-eaw_schema',
+    url='https://github.com/hvwaldow/ckanext-eaw_schema',
 
     # Author details
-    author='''Christian Foerster''',
-    author_email='''Christian.Foerster@eawag.ch''',
+    author='''Harald von Waldow''',
+    author_email='''harald.vonwaldow@eawag.ch''',
 
     # Choose your license
     license='AGPL',
@@ -45,7 +43,8 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
     ],
 
 
@@ -55,15 +54,12 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-        namespace_packages=['ckanext'],
 
-    install_requires=[
-      # CKAN extensions should not list dependencies here, but in a separate
-      # ``requirements.txt`` file.
-      #
-      # http://docs.ckan.org/en/latest/extensions/best-practices.html
-      # add-third-party-libraries-to-requirements-txt
-    ],
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
+    install_requires=[],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
@@ -74,8 +70,7 @@ setup(
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages.
-    # see http://docs.python.org/3.4/distutils/setupscript.html
-    # installing-additional-files
+    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     data_files=[],
 
@@ -84,10 +79,9 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points='''
         [ckan.plugins]
-        eaw_schema=ckanext.eaw_schema.plugin:EawSchemaPlugin
-
-        [babel.extractors]
-        ckan = ckan.lib.extract:extract_ckan
+        eaw_schema=ckanext.eaw_schema.plugin:Eaw_SchemaPlugin
+	[babel.extractors]
+	ckan = ckan.lib.extract:extract_ckan
     ''',
 
     # If you are changing from the default layout of your extension, you may
